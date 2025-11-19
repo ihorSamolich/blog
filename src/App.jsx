1615ba09a6aecc881581d3f5edc184006a6fd7f5
Ihor <ihorsamolich88@gmail.com>
2025-11-19 09:40:06 +0200

    Fix image path in BlogCard component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
         <BlogCard
-            image="/assets/images/BlogImage.png"
+            image="/blog/assets/images/BlogImage.png"
             category="HTML & CSS"
             title="Практична №3"
             description="Мета практичної роботи полягає в тому, щоб налаштувати середовище розробки VS Code, а також реалізувати картку блогу для вдосконалення фундаментальних навичок з HTML та CSS."
